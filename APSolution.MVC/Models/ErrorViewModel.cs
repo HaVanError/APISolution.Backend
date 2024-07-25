c7951c8576f73651e0b7db5fa9ac144300c80c36
HaVanError <nuahavan@gmail.com>
2024-07-25 21:09:59 +0700

    Delete APSolution.MVC directory

--- APSolution.MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace APSolution.MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
